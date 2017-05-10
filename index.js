69b574aefce6ef29b136b50bc4b1456d8cbcde1c
久之 <zuolifeng@sina.com>
2017-05-10 15:28:00 +0800

    增加了样表

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
-
-// import xlsx from 'node-xlsx';
-
-
-
-// //const workSheetFromBuffer = xlsx.parse(fs.readFileSysc(`${__dirname}/test.xlsx`));
-// const workSheetsFromFile = xlsx.parse(`${__dirname}/test.xlsx`);
-
-
-// ES5
-
 var xlsx = require('node-xlsx');
 var fs = require('fs');
 
-var obj = { "worksheets": [
-    {"data": [["name","sex","age"],["li","male","24"]] }
-]};
+const originFilename = `${__dirname}/04月汇总表.xls`;
+const originSheetname = '刷卡记录';
 
-// var file = xlsx.build(obj);
-/**
- *  ## xlsx.build(obj) 格式
- *   ```
- *   obj = [sheet1, sheet2, ...];
- *   sheet1 = { name: "sheetName", data: sheetData};
- *   sheetData = [ line1DataArray, line2DataArray, ...];
- *   line1DataArray = [data1, data2, data3, ...];
- *   ```
- *                     |------------------------|
- *   line1DataArray -> | data1 | data2| data3 |  .....|
- *                     | --------------------------|
- * 
- *                     | sheetName1 |
- */
+// 读取文件
+const originWorkSheets = xlsx.parse(fs.readFileSync(originFilename));
+// var originSheetData = null;
+// var targetSheetData = null;
 
-const workSheetsFromBuffer = xlsx.parse(fs.readFileSync(`${__dirname}/04月汇总表.xls`));
 
-console.log(JSON.stringify(workSheetsFromBuffer));
+// // 转换函数
+// // 输入原始数据中单元格的数据，拟合出目标Excel中一行的数据。
+// function convert(id,name) {
 
-// const data = [[1, 2, 3], [true, false, null, 'sheetjs'], ['foo', 'bar', new Date('2014-02-19T14:30Z'), '0.3'], ['baz', null, 'qux']];
-// var buffer = xlsx.build([{name: "mySheetName", data: data}]); // Returns a buffer
+// }
 
-// fs.writeFileSync('user.xlsx', buffer, 'binary');
+// // 获取指定sheet的数据 ，Sheet名称为：刷卡记录
+// for( var i = 0; i < originWorkSheets.length; i++) {
+//     if(originWorkSheets[i] && originWorkSheets[i].name && originWorkSheets[i].name === originSheetname ) {
+//         console.log(originWorkSheets[i].name);
+//         originSheetData = originWorkSheets[i].data;  
+//     }
+// }
+// // 开始转换
+// for( var i = 0; i < originSheetData.length; i++){
+//      console.log(JSON.stringify(originSheetData[i]));
+// }
+
+
+// // 输出转换后的格式
+// if (targetSheetData) {
+//     console.log(JSON.stringify(targetSheetData));
+// }
+//const sheet = originWorkSheets["刷卡记录"];
+
+//console.log(JSON.stringify(sheet));
+
+//const data = [[1, 2, 3], [true, false, null, 'sheetjs'], ['foo', 'bar', new Date('2014-02-19T14:30Z'), '0.3'], ['baz', null, 'qux']];
+var buffer = xlsx.build(JSON.stringify(originWorkSheets)); // Returns a buffer
+// 保存文件
+fs.writeFileSync('user1.xlsx', originWorkSheets, 'binary');
