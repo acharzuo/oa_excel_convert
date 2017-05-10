4d666ebd2d346327ecb6fbe04ff69c4a7d297bcb
久之 <zuolifeng@sina.com>
2017-05-10 18:00:58 +0800

    文件转换完成

--- index.js
+++ index.js
@@ -1,43 +1,172 @@
 var xlsx = require('node-xlsx');
 var fs = require('fs');
 
-const originFilename = `${__dirname}/04月汇总表.xls`;
+// 获取参数
+var arguments = process.argv.splice(2);
+
+// 命令行参数判断
+if(!arguments || arguments.length < 1){
+    console.log("usage: node index <Excle Filename>")
+    return;
+}
+
+
+const originFilename = `${__dirname}/` + arguments[0] ;
 const originSheetname = '刷卡记录';
 
 // 读取文件
-const originWorkSheets = xlsx.parse(fs.readFileSync(originFilename));
-// var originSheetData = null;
-// var targetSheetData = null;
+try {
+   const originWorkSheets = xlsx.parse(fs.readFileSync(originFilename)); 
+} catch (error) {
+    console.log("读取文件错误！" + originFilename);
+    return;
+}
 
+var originSheetData = null;
+var targetSheetData = null;
 
-// // 转换函数
-// // 输入原始数据中单元格的数据，拟合出目标Excel中一行的数据。
-// function convert(id,name) {
+// 人员对应规则
+const ACCOUNT_PAIR = {
+   "张涛涛":"zhangtaotao",
+    "王长顺":"a",
+    "京东陈":"b",
+    "ACHAR":"c",
+    "王思聪":"d",
+    "边得力":"e",
+    "姜孟女":"f",
+    "孙悟空":"g",
+    "曹丞相":"h",
+    "花木兰":"j",
+    "鲁智深":"z",
+    "虚竹":"x",
+    "芦沟":"cv",
+    "大川":"b",
+    "胡吗辟谷":"n",
+    "春蚕":"n",
+}
 
-// }
+// 转换函数
+// 输入原始数据Sheet的数据，拟合出目标Excel中新Sheet的数据。
+function convert(oriObj) {
 
-// // 获取指定sheet的数据 ，Sheet名称为：刷卡记录
-// for( var i = 0; i < originWorkSheets.length; i++) {
-//     if(originWorkSheets[i] && originWorkSheets[i].name && originWorkSheets[i].name === originSheetname ) {
-//         console.log(originWorkSheets[i].name);
-//         originSheetData = originWorkSheets[i].data;  
-//     }
-// }
-// // 开始转换
-// for( var i = 0; i < originSheetData.length; i++){
-//      console.log(JSON.stringify(originSheetData[i]));
-// }
+    if(typeof oriObj != "object") return;
 
+    var ret = [];
 
-// // 输出转换后的格式
-// if (targetSheetData) {
-//     console.log(JSON.stringify(targetSheetData));
-// }
-//const sheet = originWorkSheets["刷卡记录"];
+   // 一行的数据
+   var line = [
+       "",  // id	编号
+       "",  // account	用户
+       "",  // date	日期
+       "",  // signIn	签到
+       "",  // signOut	签退
+       "",  // status	状态
+       "",  // ip	IP
+       "",  // device	设备
+       "",  // manualIn	签到时间
+       "",  // manualOut	签退时间
+       "",  // reason	原因
+       "",  // desc	描述
+       "",  // reviewStatus	补录状态
+       "",  // reviewedBy	审核人
+       ""  // reviewedDate	审核时间
+   ];
 
-//console.log(JSON.stringify(sheet));
+   // 行号
+   var index = 1 ;
+   var month = "";
+   const DATE_LINE_INDEX = 3;
 
-//const data = [[1, 2, 3], [true, false, null, 'sheetjs'], ['foo', 'bar', new Date('2014-02-19T14:30Z'), '0.3'], ['baz', null, 'qux']];
-var buffer = xlsx.build(JSON.stringify(originWorkSheets)); // Returns a buffer
-// 保存文件
-fs.writeFileSync('user1.xlsx', originWorkSheets, 'binary');
+   // 遍历所有行
+   for( var i = 0; i < oriObj.length; i++){
+        // 获取考勤日期的月份
+        if( oriObj[i] && oriObj[i][0] && oriObj[i][0] === "考勤日期 : " && oriObj[i][2] ) {
+            console.log(oriObj[i][2]);
+            // 数据格式 模拟 ["2017/04/01 ~ 04/30 (ACHAR科技)"]
+            var tmp = oriObj[i][2].split(" ");
+            month = tmp[0].substr(0,7).replace("/","-");
+        } else if (oriObj[i] && oriObj[i][0] && oriObj[i][0] === "工 号：" && oriObj[i][2]) {
+            // 获取每个人每天的记录的记录
+            for(var day = 0; day < 31; day++){
+                var record = "";    // 打开记录
+                if(oriObj[i+1][day]) {
+                    record = oriObj[i+1][day];
+                    record = record.trim();
+                } else {
+                    continue;
+                }
+
+                var account = oriObj[i][10];
+                account = ACCOUNT_PAIR[account];
+                if(!account) {
+                    console.log(oriObj[i][10] + "没有对应的OA账号，已经智能跳过！");
+                    break;
+                }
+                // record = "15:45\n18:59\n"
+                var recordSplit = record.split("\n");
+                var signIn = recordSplit[0];        // 第一次打卡时间为签到时间
+                var signOut = recordSplit.length > 1 ? recordSplit[recordSplit.length-1] : "";    // 最后一次打卡时间为签退时间
+
+                // 整理字符格式，使用5位的时间表示方法
+                signIn = signIn.length === 4 ? "0" + signIn : signIn;
+                signOut = signOut.length === 4 ? "0" + signOut : signOut;
+                const WORK_IN_TIME = "08:50";
+                const WORK_OUT_TIME = "18:00";
+
+                var status = "normal";
+                if(signIn > WORK_IN_TIME && signOut < WORK_OUT_TIME && signIn != signOut ){
+                    // 迟到早退 签到 > 8:50 && 签退 < 18:00 && 签到 != 签退
+                    status = "both";
+                } else if(signIn > WORK_IN_TIME && signIn < WORK_OUT_TIME && signOut >= WORK_OUT_TIME){
+                    // 迟到判断 签到> 8:50 && 签到< 18:00 && 签退 >=18:00	
+                    status = "late";
+                } else if (signIn < WORK_IN_TIME && signOut > WORK_IN_TIME && signOut < WORK_OUT_TIME ){
+                    // 早退判断 签到<= 8:50 && 签退>8:50 && 签退<=18:00
+                    status = "early";
+                }
+
+                var line = [];
+                line[0] = index++;
+                line[1] = account;
+                line[2] = month + "-" + oriObj[DATE_LINE_INDEX][day];
+                line[3] = signIn;
+                line[4] = signOut;
+                line[5] = status;
+                line[6] = "127.0.0.1";
+                line[7] = "得力打卡机01";
+                line[8] = "00:00:00";
+                line[9] = "00:00:00";
+                line[10] = "";
+                line[11] = "";
+                line[12] = "";
+                line[13] = "";
+                line[14] = "0/0/0000 00:00:00";
+                ret.push(line);
+            }
+        }
+   }
+   
+   return ret;
+
+}
+
+// 获取指定sheet的数据 ，Sheet名称为：刷卡记录
+for( var i = 0; i < originWorkSheets.length; i++) {
+    if(originWorkSheets[i] && originWorkSheets[i].name && originWorkSheets[i].name === originSheetname ) {
+        originSheetData = originWorkSheets[i].data;  
+    }
+}
+
+// 执行转换
+targetSheetData = convert(originSheetData);
+
+// 输出转换后的格式
+if (targetSheetData) {
+    //console.log(JSON.stringify(targetSheetData));
+
+    var buffer = xlsx.build([{"name": "打卡记录", "data":targetSheetData}]); 
+    // 保存文件
+    fs.writeFileSync('user1.xlsx', buffer, 'binary');
+    console.log("转换完成！");
+}
+
